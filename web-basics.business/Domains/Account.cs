4953391c86faf2e54c73bf3d5da51a9000ef769c
Kuzmenko Andrey <kuzmenko>
2020-09-28 14:28:21 +0300

    Added domain and viewmodel for Account

---
+++ web-basics.business/Domains/Account.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+
+using Microsoft.Extensions.Configuration;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace web_basics.business.Domains
+{
+    public class Account
+    {
+        IMapper mapper;
+        data.Repositories.Account repository;
+
+        public Account(IConfiguration configuration)
+        {
+            this.repository = new data.Repositories.Account(configuration);
+            this.mapper = new MapperConfiguration(cfg => {
+                cfg.CreateMap<data.Entities.Account, ViewModels.Account>();
+            }).CreateMapper();
+        }
+
+        public IEnumerable<ViewModels.Account> Get()
+        {
+            var accounts = repository.Get();
+            return accounts.Select(account => mapper.Map<data.Entities.Account, ViewModels.Account>(account));
+        }
+    }
+}
---
+++ web-basics.business/ViewModels/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace web_basics.business.ViewModels
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public enum Role
+    {
+        User,
+        Admin
+    }
+}
